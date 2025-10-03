f4cd3443bb93b9a679010b6b4d53adb39559793f
Zehan <zehan.kareem@gmail.com>
2025-10-03 07:13:40 +0600

    Added page loader logic

--- src/index.js
+++ src/index.js
@@ -11,11 +11,24 @@ function setActiveState(lastTarget, button){
     return button;
 }
 
+function loadPage(target){
+  if(target.lastElementChild.textContent 
+    == 'Inbox' && target.classList.contains(".active")
+  ){
+    return Inbox;
+  }
+}
+
 const navButtons = document.querySelectorAll("button:not(button.notifications)");
 let lastTarget = document.querySelector("button.active:not(button.notifications)");
+const pageContent = document.querySelector(".content");
+let pageTarget;
 navButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    lastTarget = setActiveState(lastTarget, button)
+    lastTarget = setActiveState(lastTarget, button);
+    pageContent.innerHTML="";
+    pageTarget = loadPage(lastTarget);
+    pageContent.appendChild(pageTarget);
   });
 });
 

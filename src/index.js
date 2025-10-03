9f11233a499765fd2abb80be70a0045880e03800
Zehan <zehan.kareem@gmail.com>
2025-10-03 06:55:30 +0600

    Added notification icon toggle

--- src/index.js
+++ src/index.js
@@ -8,14 +8,24 @@ function setActiveState(lastTarget, button){
       button.classList.add("active");
       lastTarget.classList.remove("active")
     }
-
     return button;
 }
 
-const navButtons = document.querySelectorAll("button");
-let lastTarget = document.querySelector("button.active");
+const navButtons = document.querySelectorAll("button:not(button.notifications)");
+let lastTarget = document.querySelector("button.active:not(button.notifications)");
 navButtons.forEach((button) => {
   button.addEventListener("click", () => {
     lastTarget = setActiveState(lastTarget, button)
   });
 });
+
+const notifications = document.querySelector(".notifications");
+notifications.addEventListener("click", ()=> {
+  notifications.classList.toggle("active");
+  if (notifications.classList.contains("active")){
+    notifications.textContent = "notifications";
+  }
+  else{
+    notifications.textContent = "notifications_none";
+  }
+})

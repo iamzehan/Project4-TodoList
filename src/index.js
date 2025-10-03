7178620f4e29d2875866bdfd953c552ad923245c
Zehan <zehan.kareem@gmail.com>
2025-10-03 06:15:04 +0600

    Added active state control to navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.css';
+import "./style.css";
+
+function setActiveState(lastTarget, button){
+   if (
+      button.lastElementChild.textContent !=
+      lastTarget.lastElementChild.textContent
+    ) {
+      button.classList.add("active");
+      lastTarget.classList.remove("active")
+    }
+
+    return button;
+}
+
+const navButtons = document.querySelectorAll("button");
+let lastTarget = document.querySelector("button.active");
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    lastTarget = setActiveState(lastTarget, button)
+  });
+});

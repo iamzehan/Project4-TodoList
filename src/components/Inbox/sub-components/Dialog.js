ffca9bb7849983158e3d39b8985d26ef27dab057
Zehan <zehan.kareem@gmail.com>
2025-10-06 02:04:16 +0600

    Added extended classes

--- src/components/Inbox/sub-components/Dialog.js
+++ src/components/Inbox/sub-components/Dialog.js
@@ -3,12 +3,12 @@ import Button from "./createButton";
 // Here you basically do everything with a dialog
 
 class Dialog {
-  constructor() {
+  constructor(legend) {
     this.dialog = document.createElement("dialog");
     this.fieldset = document.createElement("fieldset"); // ✅ store as a class property
 
     this.legend = document.createElement("legend");
-    this.legend.textContent = "Add a new task";
+    this.legend.textContent = legend;
 
     this.input = document.createElement("input");
     this.input.type = "text";
@@ -26,16 +26,13 @@ class Dialog {
     this.fieldset.appendChild(this.textarea);
     this.dialog.appendChild(this.fieldset);
 
-    // Create buttons
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("btn-container");
-
-    const save = new Button(["common", "save"], "save", "Save").getButtonWithText();
+    // Create buttons container
+    this.buttonContainer = document.createElement("div");
+    this.buttonContainer.classList.add("btn-container");
     const cancel = new Button(["common", "mono", "cancel"], "cancel", "Cancel").getButtonWithText();
 
-    buttonContainer.appendChild(save);
-    buttonContainer.appendChild(cancel);
-    this.dialog.appendChild(buttonContainer);
+    this.buttonContainer.appendChild(cancel);
+    this.dialog.appendChild(this.buttonContainer);
 
     cancel.addEventListener("click", () => this.dialog.close());
   }
@@ -53,4 +50,20 @@ class Dialog {
   }
 }
 
-export {Dialog};
+class AddTask extends Dialog {
+  constructor(legend="Add New Task"){
+    super(legend);
+    const save = new Button(["common", "save"], "save", "Save").getButtonWithText();
+    this.buttonContainer.appendChild(save);
+  }
+}
+
+class UpdateTask extends Dialog {
+  constructor(data, legend="Update Task"){
+    super(legend);
+    const update = new Button(["common", "update"], "update", "Update").getButtonWithText();
+    this.buttonContainer.appendChild(update);
+  }
+}
+
+export {AddTask, UpdateTask};
